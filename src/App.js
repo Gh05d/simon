5e17aec729beb088d83793fa5c788e3965f7a037
Gh05d <pacal.clanget@googlemail.com>
2017-10-15 18:55:49 +0200

    Implemented logic for playing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Options from "./Options";
+import _ from "lodash";
+import { soundBoard } from "./sounds";
 
 class App extends Component {
   constructor(props) {
@@ -7,8 +9,25 @@ class App extends Component {
 
     this.state = {
       counter: 0,
-      strict: false
+      playerCounter: 0,
+      strict: false,
+      moves: [],
+      active: "",
+      game: false
     };
+  }
+
+  renderButtons(buttons) {
+    return buttons.map(({ value, id, color }) => (
+      <button
+        onClick={e => this.playerClick(e.target.value)}
+        value={value}
+        key={id}
+        id={`simon-button-${id}`}
+        className="w3-half"
+        style={this.state.active == value ? { background: color } : {}}
+      />
+    ));
   }
 
   onClickStrict() {
@@ -16,17 +35,75 @@ class App extends Component {
   }
 
   onClickStart() {
-    this.setState({ counter: 0 });
+    this.setState({ counter: 0, game: true });
+    this.playGame();
+  }
+
+  playGame() {
+    const simonButtons = ["upperLeft", "upperRight", "lowerLeft", "lowerRight"];
+    let currentButton = _.sample(simonButtons);
+    let currentButtons = this.state.moves;
+    currentButtons.push(currentButton);
+
+    //Save the lighted buttons so that the player can play
+    let after = _.after(currentButtons.length, () => {
+      this.setState(
+        {
+          moves: currentButtons,
+          active: "",
+          counter: this.state.counter + 1,
+          playerCounter: 0
+        },
+        console.log(currentButton)
+      );
+    });
+
+    //Highlight the selected buttons, then invoke saveMoves
+    currentButtons.map((button, counter) => {
+      if (counter == 0) {
+        counter++;
+      }
+      _.delay(() => this.setState({ active: button }, after), counter * 1000);
+    });
+  }
+
+  playerClick(pressedButton) {
+    console.log(soundBoard[0]);
+    soundBoard[0].play();
+    //Don't do anything if there is no active game
+    if (!this.state.game) {
+      return; //Check if the player has to push buttons
+    } else if (this.state.playerCounter < this.state.counter) {
+      //Check whether the player pushed the right Button
+      if (pressedButton != this.state.moves[this.state.playerCounter]) {
+        return this.setState({ game: false, moves: [] }, () =>
+          console.log("U Lost, Bro")
+        );
+      } //If he pushed the right button, increment his counter
+      this.setState({ playerCounter: this.state.playerCounter + 1 }, () => {
+        //Let the computer make the next move if the player pushed all buttons
+        if (this.state.playerCounter == this.state.counter) {
+          return this.playGame();
+        }
+      });
+    }
   }
 
   render() {
+    const simonUpperButtons = [
+      { value: "upperLeft", id: "upper-left", color: "#e00" },
+      { value: "upperRight", id: "upper-right", color: "#00d119" }
+    ];
+
+    const simonLowerButtons = [
+      { value: "lowerLeft", id: "lower-left", color: "#0017e6" },
+      { value: "lowerRight", id: "lower-right", color: "#fff000" }
+    ];
+
     return (
       <div id="wrapper">
         <div id="simon" className="w3-container" style={{ padding: "0px" }}>
-          <div className="w3-row">
-            <div className="w3-half" id="simon-button-upper-left" />
-            <div className="w3-half" id="simon-button-upper-right" />
-          </div>
+          <div className="w3-row">{this.renderButtons(simonUpperButtons)}</div>
           <div id="middle">
             <Options
               count={this.state.counter}
@@ -35,10 +112,7 @@ class App extends Component {
               clickStart={() => this.onClickStart()}
             />
           </div>
-          <div className="w3-row">
-            <div className="w3-half" id="simon-button-lower-left" />
-            <div className="w3-half" id="simon-button-lower-right" />
-          </div>
+          <div className="w3-row">{this.renderButtons(simonLowerButtons)}</div>
         </div>
       </div>
     );
---
+++ src/sounds.jsx
@@ -0,0 +1,13 @@
+const sound1 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
+);
+const sound2 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"
+);
+const sound3 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"
+);
+const sound4 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+);
+export const soundBoard = [sound1, sound2, sound3, sound4];
